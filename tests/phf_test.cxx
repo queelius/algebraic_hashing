a6d4e68f082ccb865c8c62e33b5ad157ebd37855
Alex Towell <queelius@gmail.com>
2019-01-14 07:33:48 -0600

    changed tests back to skeleton

---
+++ tests/phf_test.cxx
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <iostream>
+//#include <packed_container/bit_view.h>
+#include <vector>
+
+TEST(PHFTest, Construction)
+{
+    ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
