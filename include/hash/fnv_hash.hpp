086988f098f2579102ee364399c728ce85318d63
queelius <queelius@gmail.com>
2019-06-27 19:55:51 -0500

    test

--- include/hash/fnv_hash.hpp
+++ include/hash/fnv_hash.hpp
@@ -3,6 +3,9 @@
 #include <variant>
 #include <tuple>
 #include <optional>
+#include <string_view>
+#include <string>
+#include <typeinfo>
 
 namespace alex::hash
 {
@@ -25,10 +28,6 @@ namespace alex::hash
     };
 
     template <typename T>
-    concept bool FnvHashValue = Xorable<T> && Multipliable<T> && Convertible<T, unsigned int>;
-
-
-    template <typename T>
     concept bool Iterable() 
     {
         return requires(T x)
@@ -47,32 +46,23 @@ namespace alex::hash
 
     };
 
-    /*/template<typename T>
-    concept Hashable = requires(T x)
-    {
-        { std::hash<T>{}(x) } -> std::size_t;
-    };
-    */
-
     template <typename T>
-    concept bool FnvHashValueType = Xorable<T> && Multipliable<T> && Convertible<T, unsigned int>;
+    concept bool FnvHashValueType = Xorable<T> && Multipliable<T>; // && Convertible<T, unsigned int>;
 
     /**
      * HashValueType is a RegularType that overloads binary
      * operations * and ^ in a way that is consistent with
      * integers. It can also be constructed from integers.
      */
-    template <FnvHashValueType T = unsigned int>
+    template <typename T>
     class FnvHash
     {
     public:
-        static const T FNV_PRIME = 16777619u;
-        static const T DEFAULT_SEED = FNV_PRIME;
         using hash_value_type = T;
 
         T seed() const { return _seed; };
 
-        FnvHash(T seed = DEFAULT_SEED) : _seed(seed == 0 ? DEFAULT_SEED : seed) {};
+        FnvHash(T seed) : _seed(seed) {};
 
         /**
          * ValueType is an a type that can be statically cast to HashValueType
@@ -91,13 +81,13 @@ namespace alex::hash
         T _hash(std::tuple<V ...> const & v, T h) const
         {
             std::cout << "[tuple]\n";
-            static const T TUPLE_HASH_TAG = ;
-
-            h = _hash(TUPLE_HASH_TAG, h);
-            h = _hash(std::get<I>(v), h);
+            static const T TUPLE_TAG = 31ul;
+            
+            h = _hash(TUPLE_TAG, std::move(h));
+            h = _hash(std::get<I>(v), std::move(h));
 
             if constexpr(I+1 != sizeof...(V))
-                h = _hash<I+1>(v, h);
+                h = _hash<I+1>(v, std::move(h));
             return h;
         };
 
@@ -105,50 +95,94 @@ namespace alex::hash
         // tuple_interlace -> re-arranging the order of tuples, maybe like rename in relational algebra?
         // tuple_cat - concat two tuples. good for "flattening" them.
 
-        /*
-        template <size_t I = 0, typename... V>
-        T _hash(std::variant<V ...> const & v, T h) const
+        template <typename... V>
+        T _hash(std::variant<V ...> v, T h) const
         {
             std::cout << "[variant]\n";
-            static const T VARIANT_HASH_TAG = 1007;
+            static const T VARIANT_TAG = 1007;
+            h = _hash(VARIANT_TAG, std::move(h));
 
-            h = _hash(VARIANT_HASH_TAG, h);
-            h = _hash(std::get<I>(v), h);
-            if constexpr(I+1 != sizeof...(V))
-                h = _hash<I+1>(v, h);
+            std::visit([&h, this](const auto& x) { h = this->_hash(x, std::move(h)); }, v);
             return h;
         };
-        */
 
         template <typename V>
-        T _hash(std::optional<V> const & x, T h) const
+        T _hash(std::optional<V> x, T h) const
         {
+            std::cout << "[optional]\n";
+            const static T OPTIONAL_TAG = T(29ul);
+            h = _hash(OPTIONAL_TAG, std::move(h));
             if (x.has_value())
-                return _hash(*x, h);
-            else
-                return _hash(NULL, h);
+                return _hash(*x, std::move(h));
         };
 
-        T _hash(Iterable const & v, T h) const
+        T _hash(std::string v, T h) const
         {
-            std::cout << "[iterable]\n";
-            static const T ITERABLE_HASH_TAG = 29;
+            std::cout << "[string]\n";
+            const static T STRING_TAG = T(31ul);
 
-            h = _hash(ITERABLE_HASH_TAG, h);
-            for (auto const & x : v)
+            h = _hash(STRING_TAG, std::move(h));
+            for (auto x : v)
             {
-                h = _hash(x, h);
+                h = _hash(x, std::move(h));
             }
             return h;
         };
 
-        template <typename V> requires Convertible<V,T>
+        T _hash(int v, T h) const
+        {
+            std::cout << "[int]\n";
+            const static T INT_TAG = T(129ul);
+
+            union Tmp
+            {
+                int v;
+                char c[4];
+            };
+            Tmp tmp;
+            tmp.v = v;
+
+            h = _hash(INT_TAG, std::move(h));
+            h = _hash(tmp.c[0], std::move(h));
+            h = _hash(tmp.c[1], std::move(h));
+            h = _hash(tmp.c[2], std::move(h));
+            h = _hash(tmp.c[3], std::move(h));
+            return h;
+        };
+
+
+        T _hash(std::string_view v, T h) const
+        {
+            std::cout << "[string_view]\n";
+            const static T STRING_VIEW_TAG = T(29ul);
+
+            h = _hash(STRING_VIEW_TAG, std::move(h));
+            for (auto x : v)
+            {
+                h = _hash(x, std::move(h));
+            }
+            return h;
+        };
+
+        /*
+        template <typename V> // requires Convertible<V,T>
         T _hash(V const & v, T h) const
         {
-            std::cout << "[non-iterable]\n";
-
-            return h * FNV_PRIME ^ static_cast<T>(v);
+            const static T FNV_PRIME = T(16777619ul);
+            h = std::move(h) ^ static_cast<T>(v);
+            h = std::move(h) * FNV_PRIME;
+            return h;
         };
+        */
+
+        T _hash(char x, T h) const
+        {
+            const static T FNV_PRIME = T(16777619ul);
+            h = std::move(h) ^ static_cast<T>(x);
+            h = std::move(h) * FNV_PRIME;
+            return h;
+        };
+
 
     };
 }
--- include/set/perf_hash_filter.hpp
+++ include/set/perf_hash_filter.hpp
@@ -8,60 +8,128 @@ namespace alex::set
         X operator()(X x) { return x };
     };
 
-    // Hash : Hashable -> HashValueType
-    // PerfHash : Hashable -> [0,...,N-1]
-    // Coder : ValueType -> Hashable
-    // Default: Coder is identity for ValueType
-    //          which will work if ValueType is a
-    //          Hashable type.
-
     /**
+     * Hash : Hashable -> HashType
+     * PerfHash : Hashable -> [0,N)
+     * Coder : T -> Hashable
+     * Default: Coder is identity on T, in which
+     *          case T must satisfy Hashable.
+     * 
      * If Coder is not injective, then two or more elements
-     * of ValueType map to the same element in HashValueType.
-     * These creates an equivelance relation, i.e., if
-     * Coder(x) = Coder(y) then x equiv y. This could potentially
-     * reduce the bit length of the PerfHash object since
-     * given a set S to perfectly hash, it only needs...
+     * of T map to the same element in HashType, which
+     * induces an equivelance relation such that the perfect
+     * hash filter is not over T but the quotient set given by
+     * T \ Coder.
      * 
-     * If accidental, then increases fpr rate.
+     * If this was not intended, then Coder separately increases
+     * the false positive rate.
      * 
-     * template parameter for fpr through N?
+     * The default Coder is the identity on ValueType, in which
+     * case ValueType must be Hashable.
+     * 
+     * Todo: make N a template parameter, and thus fpr a template?
      */
     template <
-        typename ValueType,
+        typename T,
         typename PerfHash,
         typename Hash,
-        typename Interval,
-        typename Coder = Identity<ValueType>>
+        typename Coder = Identity<T>>
     class PerfectHashFilter
     {
     public:
+        using value_type = T;
+
+        using hash_fn = Hash;
+
+        auto perfect_hash(T const & x) const
+        {
+            return _ph(_coder(x));
+        }
+
         template <typename I>
         PerfectHashFilter(
             I begin,
             I end,
-            Interval fpr,
-            Interval fnr,
-            double r)
+            long double fpr,
+            double load_factor = 0.99,
+            Coder coder = Coder{})
         {
-
+                        
         };
 
-        bool contains(ValueType x) const
+        bool contains(T const & x) const
         {
-            auto code = Encoder(x);
-            return _hashes[_ph(code)] == _h(code) % _N;
+            auto code = _coder(x);
+            auto index = perf_hash = _ph(code);
+            auto hash = _h(code) % _N;
+            
+            return _hashes[index] == hash;
+        };
+
+        long double fpr() const
+        {
+            return 1 / static_cast<long double>(_N);
+        };
+
+        // If ValueType is a countably infinite set, then
+        //     representational equality => equality and
+        //     equality => representational equality.
+        // If ValueType is finite, then
+        //     representational equality => equality but
+        //     equality does not imply representational equality.
+        // In particular, two PHFs X, Y may have different representations
+        // but have the same output on any regular function for
+        // approximate sets, e.g., contains(X,a) = contains(Y,a) for all
+        // a in the finite universe. Note that if the function is not
+        // regular, e.g., if load_factor or size of objective set was
+        // stored in the PHF and used in the function, two PHFs that
+        // behave the same with respect to functions like the member-of
+        // predicate may not behave the same with non-regular functions
+        // that compute output based on load_factor or objective set size.
+        bool operator==(
+            PerfectHashFilter<ValueType, PerfHash, Hash, Coder> const & other) const
+        {
+            if (_N != other._N ||
+                _h != other._h ||
+                _ph != other._ph ||
+                _coder == other._coder ||
+                _hashes.size() != other._hashes.size())
+            {
+                return false;
+            }
+
+            for (size_t i = 0; i < _hashes.size(); ++i)
+            {
+                if (_hashes[i] != other._hashes[i])
+                    return false;
+            }
+            return true;
+        };
+
+        // If ValueType is a countably infinite set, then no perfect hash filter is a subset of another
+        // perfect hash filter. (Every perfect hash filter is a subset of the universal set for ValueType.)
+        //
+        // Note that when we incorporate set-theoretic operations, like differences and intersections,
+        // then subsets are possible given a countably infinite universe. For instance, suppose X and Y
+        // are PHFs, then X \ Y is a subset of X.
+        constexpr bool subset(
+            PerfectHashFilter<ValueType, PerfHash, Hash, Coder> const & other) const
+        {
+            return false;
         };
 
     private:
-        Interval fpr, fnr;
+        using hash_type = typename Hash::hash_type;
+
         PerfHash _ph;
         Hash _h;
-        typename Hash::hash_type _N;
+        hash_type _N;
+        Coder _coder;
 
         // this should be a packed container type
-        // that (assuming uniform distribution)
-        // [0,N-1] can be compactly stored.
-        std::vector<typename Hash::hash_type> _hashes;
+        // that (assuming discrete uniform distribution
+        // [0,N)) that can be compactly stored
+        // in ceil(|S|/r log2 N) bits.
+        std::vector<hash_type> _hashes;
     };
 }
