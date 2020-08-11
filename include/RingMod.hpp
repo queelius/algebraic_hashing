d33cf19593ede0a497df6d036884f067c11a85ec
Alex Towell <queelius@gmail.com>
2020-08-10 21:54:53 -0500

    updates

--- include/RingMod.hpp
+++ include/RingMod.hpp
@@ -3,257 +3,206 @@
 #include <array>
 #include <string.h>
 #include <cmath>
+#include <limits>
+using std::array;
 
 /**
- * RingMod<N> is integer modulo 2^N.
- * 
- * Most of the properties of integer modulo k
- * are not implemented, only enough to be useful
- * as a set of hash values of arbitrary length
- * with multiplication, addition, and xor.
- * 
+ * mod<N> is integer modulo 2^N over {+,*,^}.
+ * That is, it is a ring
+ *     (mod<N>, *, +, ~, mod<N>(), mod<N>::max())
+ *
  * String literals for constructing common modulo
  * equivalence classes are provided, e.g.,
- *     101_mod16 => RingMod<4>(5).
+ *     101_mod16 => mod<4>(5).
  * 
- * RingMod<N> is a RegularType that overloads the
- * operator set {+, *, ^, ==, <}.
+ * It is a lattice, i.e., totally ordered?
  */
 
-namespace alex::ringmod
+namespace alex::math
 {
-    // ringmod<N> is integer modulo 2^N
     template <size_t N>
-    struct RingMod
+    struct mod
     {
-        auto begin() const { return std::begin(digits); };
-        auto end() const { return std::end(digits); };
-        auto log_mod() { return N; };
+        static_assert(N != 0);
 
-        RingMod(unsigned int n = 0);
-        RingMod(char const*);
-
-        RingMod(std::array<bool, N> x = {}) : digits(x) {};
-
-        operator unsigned int() const { return convert<unsigned int>(); };
-        operator unsigned long long() const { return convert<unsigned long long>(); };
-        operator unsigned long() const { return convert<unsigned long>(); };
+        auto begin() const { return std::begin(digits); }
+        auto end() const { return std::end(digits); }
+        constexpr auto n() { return static_cast<size_t>(1)<<N; }
 
         template <typename T>
-        T convert() const
+        mod(T n)
         {
-            T result = T(0);
-            for (size_t i = digits.size() - 1; i < digits.size(); --i)
-                result = T(2) * result + digits[i];
-            return result;
-        };
+            size_t i = 0;
+            while (n != 0ul && i != N)
+                { digits[i++] = n % 2; n /= 2; }
+        }
 
+        auto & operator[](size_t d) { return digits[d]; }
+        auto operator()(size_t d) const { return digits[d]; }
 
-        std::array<bool,N> digits;
-    };
-
-    template <size_t N>
-    bool odd(RingMod<N> const &);
-
-    template <size_t N1, size_t N2>
-    bool operator==(RingMod<N1> const &, RingMod<N2> const &);
-
-    template <size_t N>
-    bool operator<(RingMod<N> const &, RingMod<N> const &);
-
-    template <size_t N>
-    RingMod<N> operator+(RingMod<N> const &, RingMod<N> const &);
-
-    template <size_t N>
-    RingMod<N> operator^(RingMod<N> const &, RingMod<N> const &);
-
-    template <size_t N>
-    RingMod<N> operator*(RingMod<N> const &, RingMod<N> const &);
-
-    RingMod<2> operator "" _mod4(const char*);
-
-    RingMod<4> operator "" _mod16(const char*);
-
-    RingMod<10> operator "" _mod10b(const char*);
-
-    RingMod<512> operator "" _mod512b(const char*);
-}
-
-// implementation
-namespace alex::ringmod
-{
-    // implementation details. not part of the public interface.
-    namespace detail
-    {
-        // preconditions: x is a binary sequence of 0's or 1's.
-        template <size_t N>
-        constexpr std::array<bool,N> from_binary(const char* x)
+        // interprets as a string representing a binary number (base-2).
+        // if a digit is '0' then interprets as 0 otherwise interprets as 1.
+        mod(char const*)
         {
-            std::array<bool, N> a{};
             auto n = strlen(x);
             if (n <= N)
             {
                 for (size_t i = 0; i < n; ++i)
                 {
-                    if (x[i] == '\0')
-                        break;
-                    // assert(x[i] == '0' || x[i] == '1');
-                    a[n-i-1] = (bool)(x[i] - '0');
+                    if (x[i] == '\0') break;
+                    digits[n-i-1] = (x[i] == '0');
                 }
             }
             else
             {
                 for (size_t i = 0; i < N; ++i)
                 {
-                    if (x[i] == '\0')
-                        break;
-                    // assert(x[i] == '0' || x[i] == '1');
-                    a[N-i-1] = (bool)(x[n-N+i] - '0');
+                    if (x[i] == '\0') break;
+                    digits[N-i-1] = x[n-N+i] == '0';
                 }               
             }
             
             return a;
-        };
+        }
 
-        template <size_t N>
-        constexpr std::array<bool,N> from_int(unsigned long long n)
-        {
-            std::array<bool,N> a{};
-            size_t i = 0;
-            while (n != 0ul && i != N)
-            {
-                a[i] = n % 2;
-                ++i;
-                n /= 2;
-            }
-            return a;
-        };
+        mod(array<bool,N> x) : digits(x) {}
+        mod() = default;
 
-        template <size_t N>
-        constexpr std::array<bool,N> xor_op(std::array<bool,N> const & a, std::array<bool,N> const & b)
-        {
-            std::array<bool,N> c{};
-            for(size_t i = 0; i < N; ++i)
-                c[i] = a[i] ^ b[i];
-            return c;
-        };
+        operator int() const { return convert<int>(); }
+        operator unsigned int() const { return convert<unsigned int>(); }
+        operator unsigned long long() const { return convert<unsigned long long>(); }
+        operator unsigned long() const { return convert<unsigned long>(); }
+        operator size_t() const { return convert<size_t>(); }
 
-        template <size_t N>
-        constexpr std::array<bool,N> add_op(std::array<bool,N> const & a, std::array<bool,N> const & b)
+        template <typename T>
+        T convert() const
         {
-            std::array<bool,N> c{};
-            bool carry = false;
-            for(size_t i = 0; i < N; ++i)
-            {
-                if (a[i])
-                {
-                    if (b[i])
-                    {
-                        c[i] = carry;
-                        carry = true;
-                    }
-                    else
-                        c[i] = !carry;
-                }
-                else
-                {
-                    if (b[i])
-                        c[i] = !carry;
-                    else
-                    {
-                        c[i] = carry;
-                        carry = false;
-                    }
-                }
-            }
-            return c;
-        };
+            T x = T(0);
+            for (size_t i = digits.size() - 1; i < digits.size(); --i)
+                x = T(2) * x + digits[i];
+            return x;
+        }
 
-        template <size_t N>
-        constexpr std::array<bool,N> multiply_op(std::array<bool,N> const & a, std::array<bool,N> const & b)
-        {
-            std::array<bool,N> c{};
-            for(size_t i = 0; i < N; ++i)
-            {
-                std::array<bool,N> k{};
-                for(size_t j = 0; j < N-i; ++j)
-                    k[j+i] = a[i] && b[j];
-                c = std::move(add_op(c, k));
-            }
-            return c;
-        };
+        array<bool,N> digits;
+    };
+
+    template <size_t N>
+    bool odd(mod<N> const &) { return !a(0); }
+
+    template <size_t N>
+    bool even(mod<N> const & a) { return a(0); }
+
+    template <size_t N>
+    mod<N> half(mod<N> a)
+    {
+        for (size_t i = N-1; i < N; --i)
+            if (a(i) == true) { a[i] = false; break; }
+        return a;
     }
 
     template <size_t N>
-    RingMod<N>::RingMod(unsigned int n) : digits(detail::from_int<N>(n))
-    {
-        // do nothing else
-    }
-
-    template <size_t N>
-    RingMod<N>::RingMod(char const* x) : digits(detail::from_binary<N>(x))
-    {
-        // do nothing else
-    }
-
-    template <size_t N>
-    bool odd(RingMod<N> const & a)
-    {
-        return a[0];
-    }
-
-    template <size_t N1, size_t N2>
-    bool operator==(RingMod<N1> const & a, RingMod<N2> const & b)
+    bool operator==(mod<N> const & a, mod<N> const & b)
     {
         return a.digits == b.digits;
     }
 
     template <size_t N>
-    bool operator<(RingMod<N> const & a, RingMod<N> const & b)
+    bool operator<(mod<N> const & a, mod<N> const & b)
     {
         for (size_t i=N-1; i < N; --i)
-        {
-            if (a.digits[i] != b.digits[i])
-                return b.digits[i];
-        }
+            if (a(i) != b(i)) return b(i);
         return false;
     }
 
     template <size_t N>
-    RingMod<N> operator+(RingMod<N> const & a, RingMod<N> const & b)
+    mod<N> operator+(mod<N> a, mod<N> const & b)
     {
-        return RingMod<N>(detail::add_op(a.digits, b.digits));
+        bool carry = false;
+        for(size_t i = 0; i < N; ++i)
+        {
+            if (a(i))
+            {
+                if (b(i))
+                    { a[i] = carry; carry = true; }
+                else
+                    a[i] = !carry;
+            }
+            else
+            {
+                if (b(i))
+                    a[i] = !carry;
+                else
+                    { a[i] = carry; carry = false; }
+            }
+        }
+        return a;
+
     }
 
     template <size_t N>
-    RingMod<N> operator^(RingMod<N> const & a, RingMod<N> const & b)
+    mod<N> operator^(mod<N> a, mod<N> const & b)
     {
-        return RingMod<N>(detail::xor_op(a.digits, b.digits));
+        for(size_t i = 0; i < N; ++i)
+            a[i] ^= b(i);
+        return a;
     }
 
     template <size_t N>
-    RingMod<N> operator*(RingMod<N> const & a, RingMod<N> const & b)
+    mod<N> operator*(mod<N> const & a, mod<N> const & b) noexcept
     {
-        return RingMod<N>(detail::multiply_op(a.digits, b.digits));
+        mod<N> c;
+        for(size_t i = 0; i < N; ++i)
+        {
+            mod<N> k;
+            for(size_t j = 0; j < N-i; ++j)
+                k[j+i] = a(i) && b(j);
+            c = c + k;
+        }
+        return c;
     }
 
-    RingMod<2> operator "" _mod4(const char* x)
+    template <size_t N> struct std::hash<mod<N>>
     {
-        return RingMod<2>(x);
-    }
+        std::size_t operator()(mod<N> const & x) const noexcept
+        {
+            constexpr size_t bsize = 8*sizeof(size_t);
+            size_t hs = 0;
+            size_t h;
+            for (size_t j = 0; j < N / bsize; ++j)
+            {
+                h = 0;
+                for (size_t i = 0; i < bsize; ++i)
+                    h = 2 * h + x(i);
+                hs ^= hash<size_t>(h);
+            }
+            h = 0;
+            for (size_t i = 0; i < N % bsize; ++i)
+                h = 2 * h + x(i);
+            hs ^= hash<size_t>(h);
 
-    RingMod<4> operator "" _mod16(const char* x)
-    {
-        return RingMod<4>(x);
-    }
+            return hs;
+        }
+    };
 
-    RingMod<10> operator "" _mod10b(const char* x)
-    {
-        return RingMod<10>(x);
-    }
+    mod<1> operator "" _mod2(char const * x) { return mod<1>(x); }
+    mod<2> operator "" _mod4(char const * x) { return mod<2>(x); }
+    mod<4> operator "" _mod16(char const * x) { return mod<4>(x); }
+    mod<8> operator "" _mod256(char const * x) { return mod<8>(x); }
+    mod<10> operator "" _mod10b(char const * x) { return mod<10>(x); }
+    mod<16> operator "" _mod16b(char const * x) { return mod<16>(x); }
+    mod<32> operator "" _mod32b(char const * x) { return mod<32>(x); }
+    mod<64> operator "" _mod64b(char const * x) { return mod<64>(x); }
+    mod<128> operator "" _mod128(char const * x) { return mod<126>(x); }
+    mod<256> operator "" _mod256(char const * x) { return mod<256>(x); }
+    mod<512> operator "" _mod512b(char const * x) { return mod<512>(x); }
+    mod<1024> operator "" _mod1kb(char const * x) { return mod<1024>(x); }
+    mod<2048> operator "" _mod2kb(char const * x) { return mod<2048>(x); }
 
-    RingMod<512> operator "" _mod512b(const char* x)
+    template <size_t N>
+    class std::numeric_limits<mod<N>>
     {
-        return RingMod<512>(x);
-    }
+    public:
+        static mod<N> lowest() { return mod<N>() }
+        static mod<N> max() { return mod<N>(array<bool,N>{1}); }
+    };    
 }
